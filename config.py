f64228442f24490b5254abf62ca14f4328839468
TNIFL <tnifl@naver.com>
2025-10-12 20:50:22 +0900

    update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+# config.py
 import os
-
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///app.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # Railway의 Postgres URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # 연결 안정성(권장)
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 280,
+    }
